af0ecb42ae6d0f587c6b6d1a700c4237f3ed61fd
libinbin <libinbin@rhtop.cn>
2018-05-23 17:06:29 +0800

    sql

---
+++ sql.sql
@@ -0,0 +1,23 @@
+INSERT INTO vehcile_platform.sys_zdlm (LMDM, LMMC, CJR, XGSJ, XGR, QZ, CJSJ) VALUES ('HPZL', '车辆类型', '1-超级管理员', '0000-00-00 00:00:00', null, null, '2018-05-22 14:45:43');
+INSERT INTO vehcile_platform.sys_zdlm (LMDM, LMMC, CJR, XGSJ, XGR, QZ, CJSJ) VALUES ('SYXZ', '使用性质', '1-超级管理员', '0000-00-00 00:00:00', null, null, '2018-05-22 14:45:08');
+INSERT INTO vehcile_platform.sys_zdlm (LMDM, LMMC, CJR, XGSJ, XGR, QZ, CJSJ) VALUES ('ylCzlx', '油卡操作类型', '1-超级管理员', '0000-00-00 00:00:00', null, null, '2018-05-19 16:56:18');
+INSERT INTO vehcile_platform.sys_zdlm (LMDM, LMMC, CJR, XGSJ, XGR, QZ, CJSJ) VALUES ('yllx', '油料类型', '1-超级管理员', '0000-00-00 00:00:00', null, null, '2018-05-19 16:44:05');
+
+
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439481952272384', 'yllx', 'q89', '89#汽油', null, '1-超级管理员', null, null, null, '2018-05-19 16:44:45');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439530643947520', 'yllx', 'q92', '92#汽油', null, '1-超级管理员', null, null, null, '2018-05-19 16:44:57');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439778900606976', 'yllx', 'q95', '95#汽油', null, '1-超级管理员', null, null, null, '2018-05-19 16:45:56');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439834940702720', 'yllx', 'q98', '98#汽油', null, '1-超级管理员', null, null, null, '2018-05-19 16:46:09');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439882718019584', 'yllx', 'q101', '101#汽油', null, '1-超级管理员', null, null, null, '2018-05-19 16:46:21');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439923146915840', 'yllx', 'c0', '0#柴油', null, '1-超级管理员', null, null, null, '2018-05-19 16:46:30');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447439970081177600', 'yllx', 'c10', '10#柴油', null, '1-超级管理员', null, null, null, '2018-05-19 16:46:41');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447440001228079104', 'yllx', 'c20', '20#柴油', null, '1-超级管理员', null, null, null, '2018-05-19 16:46:49');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447440036686725120', 'yllx', 'c30', '30#柴油', null, '1-超级管理员', null, null, null, '2018-05-19 16:46:57');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447440074108305408', 'yllx', 'yc', '乙醇', null, '1-超级管理员', null, null, null, '2018-05-19 16:47:06');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447440110196097024', 'yllx', 'trq', '天然气', null, '1-超级管理员', null, null, null, '2018-05-19 16:47:15');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447442429809786880', 'ylczlx', '10', '充值', null, '1-超级管理员', null, null, null, '2018-05-19 16:56:28');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('447442456984682496', 'ylczlx', '20', '消费', null, '1-超级管理员', null, null, null, '2018-05-19 16:56:34');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('448496615037075456', 'SYXZ', '10', '非营运机动车', null, '1-超级管理员', null, null, null, '2018-05-22 14:45:25');
+INSERT INTO vehcile_platform.sys_zdxm (ZD_ID, ZDLMDM, ZDDM, ZDMC, QZ, CJR, BY1, BY2, BY3, CJSJ) VALUES ('448496749514850304', 'HPZL', '20', '小型汽车', null, '1-超级管理员', null, null, null, '2018-05-22 14:45:57');
+
+
