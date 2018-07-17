2f028dbcb2be1839ae8fc8d1e950fafd18b1da68
chenwei <chenwei@gongyingshi.com>
2018-07-17 22:14:07 +0800

    优化

--- web-page/src/axios/api.js
+++ web-page/src/axios/api.js
@@ -1,15 +1,15 @@
 //网络请求地址前缀 "http://127.0.0.1";//
-let BASE_URL = 'http://119.97.172.181:8080';
+let BASE_URL = 'http://119.97.172.181:8180';
 //let BASE_URL = 'http://119.97.172.181'';
 module.exports = {
     //url: BASE_URL + ':8180',
     url: BASE_URL,
     //图片上传地址
-    UPLOAD: 'http://119.97.172.181:8080' + '/upload',
-    //STATIC_PATH: BASE_URL + '/image/',
-    STATIC_PATH: 'http://119.97.172.181' + ':9001/',
+    UPLOAD: 'http://119.97.172.181:8180' + '/upload',
+    STATIC_PATH: 'http://119.97.172.181/image/',
+    // STATIC_PATH: 'http://119.97.172.181' + ':9001/',
     VIDEO_PATH:'',
-    //文件控制
+    //文件控制119.97.172.181
     FILE:{
         FINDBYPID:BASE_URL+'/api/files/findByPId',//根据主ID查询文件信息
         UPLOAD:BASE_URL+ '/api/files/upload',//根据主ID查询文件信息
